ed336b1d04ee3213153351df9df408c5b53e3072
sbx_user1051 <sbx_user1051@169.254.75.69>
2024-07-25 20:40:41 +0000

    Thu Jul 25 20:40:41 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,7 +4,18 @@ from collections import defaultdict
 # skus = unicode string
 def checkout(skus):
     items = ("A", "B", "C", "D")
-    num_items = defaultdict(list)
+    num_items = {item: 0 for item in items}
+    costs = {"A": 50, "B": 30, "C": 20, "D": 15}
+    for item in skus:
+        if not item in num_items:
+            return -1
+        num_items[item] += 1
 
-    raise NotImplementedError()
+    # capture how many lots of 3 As there are
+    three_as = int(num_items["A"] / 3)
+    three_as_discount = 20
+    two_bs = int(num_items["B"] / 2)
+    two_bs_discount = 15
+    price = sum(num_items[item] * costs[item] for item in num_items) - three_as * three_as_discount - two_bs * two_bs_discount
 
+    return price
--- test/solution_tests/NEW_TESTS/test_start.py
+++ test/solution_tests/NEW_TESTS/test_start.py
@@ -1,6 +1,11 @@
 from solutions.CHK import checkout_solution
 
 class TestCheckout():
-    def test_checkout():
-        checkout_solution.checkout(123)
-
+    def test_checkout(self):
+        assert checkout_solution.checkout("ABC") == 100
+        assert checkout_solution.checkout("AAA") == 130
+        assert checkout_solution.checkout("BB") == 45
+        assert checkout_solution.checkout("BBB") == 75
+        assert checkout_solution.checkout("AAAABBB") == 255
+        assert checkout_solution.checkout("AAAABBBDDC") == 305
+        assert checkout_solution.checkout("AAAABBB,DDC") == -1
