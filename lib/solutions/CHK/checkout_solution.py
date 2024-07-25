3646b21cb085bd8d15543268c9f5f4e0e61ec71e
sbx_user1051 <sbx_user1051@169.254.75.69>
2024-07-25 21:13:44 +0000

    Thu Jul 25 21:13:44 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,6 +10,26 @@ def checkout(skus):
         "D": 15,
         "E": 40,
         "F": 10,
+        "G": 20,
+        "H": 10,
+        "I": 35,
+        "J": 60,
+        "K": 80,
+        "L": 90,
+        "M": 15,
+        "N": 40,
+        "O": 10,
+        "P": 50,
+        "Q": 30,
+        "R": 50,
+        "S": 30,
+        "T": 20,
+        "U": 40,
+        "V": 50,
+        "W": 20,
+        "X": 90,
+        "Y": 10,
+        "Z": 50,
     }
 
     # multibuy discounts. List in the order that the discounts should be applied
@@ -39,12 +59,15 @@ def checkout(skus):
             return -1
         num_items[item] += 1
 
-    # apply freebies
+    # apply freebies first - this works out best for customer
     for item, free_info in freebies:
         num_req, free_item = free_info
         free_count = int(num_items[item] / num_req)
         num_items[free_item] = max(num_items[free_item] - free_count, 0)
 
+    # apply discounts
+    for item, discount_info in discounts:
+        discount_for_item[item] = calc_discount_for_item(num_items[item], discount_info)
 
     return price
 
@@ -58,6 +81,7 @@ def calc_as_price(num_as, cost_as):
         price_as -= 20
 
     return price_as
+
 
 
 
