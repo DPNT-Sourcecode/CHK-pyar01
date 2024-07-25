0957fd68164c8659c0f7204c7df9181af46cf23f
sbx_user1051 <sbx_user1051@169.254.75.69>
2024-07-25 20:53:05 +0000

    Thu Jul 25 20:53:05 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,20 +15,25 @@ def checkout(skus):
     free_bs = int(num_items["E"] / 2)
     num_items["B"] = max(num_items["B"] - free_bs, 0)
 
-    # pricing structure means we can calculate the number of 5As first.
-    # then if there's 0-2 left over, no extra discount, or if there's 3-4 left over, apply a 20 discount
-    five_as = int(num_items["A"] / 5)
-    leftover_as = num_items["A"] - 5 * five_as
-    price_as = five_as * 200 + leftover_as * costs["A"]
-    if leftover_as >= 3:
-        price_as -= 20
-
+    price_as = calc_as_price(num_items["A"], costs["A"])
 
     # apply Bs discount
     two_bs = int(num_items["B"] / 2)
     two_bs_discount = 15
-    price = sum(num_items[item] * costs[item] for item in num_items) - three_as * three_as_discount - two_bs * two_bs_discount
+
+    price = sum(num_items[item] * costs[item] for item in num_items if item != "A") - (two_bs * two_bs_discount) + price_as
 
     return price
+
+def calc_as_price(num_as, cost_as):
+    # pricing structure means we can calculate the number of 5As first, and charge for those
+    # then if there's 0-2 left over, no extra discount, or if there's 3-4 left over, apply a 20 discount
+    five_as = int(num_as / 5)
+    leftover_as = num_as - 5 * five_as
+    price_as = five_as * 200 + leftover_as * cost_as
+    if leftover_as >= 3:
+        price_as -= 20
+
+    return price_as
 
 
--- test/solution_tests/NEW_TESTS/test_start.py
+++ test/solution_tests/NEW_TESTS/test_start.py
@@ -9,3 +9,8 @@ class TestCheckout():
         assert checkout_solution.checkout("AAAABBB") == 255
         assert checkout_solution.checkout("AAAABBBDDC") == 305
         assert checkout_solution.checkout("AAAABBB,DDC") == -1
+        assert checkout_solution.checkout("AAAAA") == 200
+        assert checkout_solution.checkout("AAAAAA") == 200
+        assert checkout_solution.checkout("AAAAAAA") == 200
+        assert checkout_solution.checkout("AAAAAAAA") == 200
+        assert checkout_solution.checkout("AAAAAAAAA") == 200
